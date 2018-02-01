6749a7edce8c170eaa3535e63cda50158277be79
SilverSpectre <gweber@student.neumont.edu>
2018-02-01 03:17:32 -0700

    Added the template code for the slideshow in script, added to the tble to fit requirements

--- scripts/script.js
+++ scripts/script.js
@@ -14,4 +14,21 @@ $(document).ready(function() {
             }
         });
     });
+    //Slideshow code
+    var image = document.getElementById("image");
+ 
+    var imageArray = [/*Set images here*/];
+ 
+    var imageIndex = 0;
+ 
+    function changeImage() {
+        image.setAttribute("src",imageArray[imageIndex]);
+        imageIndex++;
+        if (imageIndex >= imageArray.length) {
+            imageIndex = 0;
+        }
+    }
+    window.onload = function() {
+        setInterval(changeImage, 2000);
+    }
 });
