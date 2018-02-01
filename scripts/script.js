e384ef049407cdcf7166bb64d4aaeb9d41e7f4ce
Fermin <ferminsandoval950@hotmail.com>
2018-01-31 20:01:53 -0700

    Added contact information and info enlarges when clicked

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
 
     $('#nav-placeholder').load("nav.html");
+    $('#1, #2, #3').each(function() {
+        var changeSize = false;
+        $(this).click(function() {
+            if(!changeSize) {
+                $(this).animate({fontSize: "22px"}, 100);
+                changeSize = true;
+            }
+            else {
+                $(this).animate({fontSize: "16px"}, 100);
+                changeSize = false;
+            }
+        });
+    });
 });
